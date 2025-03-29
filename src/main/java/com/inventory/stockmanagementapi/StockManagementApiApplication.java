761eb62043eef0708f1e736faa66935acd7e283b
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-03-29 17:55:13 +0100

    Activation du scheduling dans l'application Spring Boot

--- src/main/java/com/inventory/stockmanagementapi/StockManagementApiApplication.java
+++ src/main/java/com/inventory/stockmanagementapi/StockManagementApiApplication.java
@@ -2,10 +2,12 @@ package com.inventory.stockmanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableScheduling
 @OpenAPIDefinition(
     info = @Info(
         title = "Stock Management API",
