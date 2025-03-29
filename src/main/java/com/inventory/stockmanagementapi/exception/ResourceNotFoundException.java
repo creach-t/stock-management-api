018a2f53bfc9df50d7ae489150a5562c612e4841
CREACH-T <142243266+creach-t@users.noreply.github.com>
2025-03-29 09:40:46 +0100

    Add ResourceNotFoundException class

---
+++ src/main/java/com/inventory/stockmanagementapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.inventory.stockmanagementapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    public ResourceNotFoundException(String resourceType, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s: '%s'", resourceType, fieldName, fieldValue));
+    }
+}
